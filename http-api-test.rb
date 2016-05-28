0d3f2780d951833b0e34b94c6d92917a420244fd
Mason Pierce <masopie@gmail.com>
2016-05-28 12:35:01 -0500

    Printarino

--- http-api-test.rb
+++ http-api-test.rb
@@ -1,6 +1,6 @@
 require "net/http"
 
-url = 'http://www.acme.com/products/3322'
+url = 'http://www.hackertyper.com'
 response = Net::HTTP.get_response(URI.parse(url))
 
 response_text = response.body
