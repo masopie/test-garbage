bef5bd30e19e762bf32b9ebcf8db2d062d8f0d4b
Mason Pierce <masopie@gmail.com>
2016-05-28 20:04:45 -0500

    P out

--- http-api-test.rb
+++ http-api-test.rb
@@ -2,7 +2,13 @@ require "net/http"
 require "uri"
 
 uri = URI.parse("http://hackertyper.com/")
+  p uri
+
 response = Net::HTTP.get_response(uri)
+  p response
+  response.each do |a|
+    p a
+  end
 
 response_text = response.body
-puts response_text
+p response_text
